f8992f6faaa4915af473d3abd93dd28fc8e814f2
Tis Lais <tistististis@gmail.com>
2021-08-08 13:20:40 -0700

    Add post route test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,29 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
+const machine = {
+  name: 'Medieval Madness',
+  manufacturer: 'Williams',
+  image: 'url',
+  date: 'today',
+  type: 'solid state'
+};
+
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('does something', async () => {
+    const res = await request(app)
+      .post('/api/v1/machines')
+      .send(machine);
+
+    expect(res.body).toEqual({
+      id: 1,
+      ...machine
+    });
+  });
+  
 });
+
