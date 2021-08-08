da58deba42389dbf2a1ee2b1e769020d7eba0fa9
Tis Lais <tistististis@gmail.com>
2021-08-08 14:00:29 -0700

    Add find all machines test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,30 +2,48 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Machine from '../lib/models/Machine.js';
 
-const machine = {
-  name: 'Medieval Madness',
-  manufacturer: 'Williams',
-  image: 'url',
-  date: 'today',
-  type: 'solid state'
-};
+const machines = [
+    {
+    name: 'Medieval Madness',
+    manufacturer: 'Williams',
+    image: 'url',
+    date: 'today',
+    type: 'solid state'
+  },
+  {
+    name: 'The Addams Family',
+    manufacturer: 'Williams',
+    image: 'url',
+    date: 'today',
+    type: 'solid state'
+  }
+];
 
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('does something', async () => {
+  it('adds a new machine to our database', async () => {
     
     const res = await request(app)
       .post('/api/v1/machines')
-      .send(machine);
+      .send(machines[0]);
 
     expect(res.body).toEqual({
       id: 1,
-      ...machine
+      ...machines[0]
     });
+  });
+
+  it.only('finds all machines in our database', async () => {
+    await Machine.insert(machines[0]);
+    await Machine.insert(machines[1]);
+    const res = await request(app).get('/api/v1/machines');
+
+    expect(res.body).toEqual(machines);
   });
 
 });
